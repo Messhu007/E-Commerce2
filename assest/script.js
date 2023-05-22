66943f558d5124b5fe37f8d514793ffca667615c
Mahesh Nagar <118973177+Messhu007@users.noreply.github.com>
2023-05-22 13:22:01 +0530

    Add files via upload

---
+++ assest/script.js
@@ -0,0 +1,214 @@
+$('.slider-01').owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: false,
+        dots: false,
+        ltr: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            100: {
+                items: 1
+            },
+            500: {
+                items: 1
+            },
+            768: {
+                items: 1
+            },
+
+            1100: {
+                items: 1
+            },
+
+            1300: {
+                items: 1
+            }
+        }
+    })
+
+    $('.slider-02').owlCarousel({
+        loop: true,
+        margin: 8,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        autoplayHoverPause: true,
+        responsive: {
+            100: {
+                items: 1
+            },
+            320: {
+                items: 2
+            },
+            500: {
+                items: 2
+            },
+            768: {
+                items: 4
+            },
+
+            1100: {
+                items: 4
+            },
+
+            1300: {
+                items: 5
+            }
+        }
+    })
+    
+    $('.slider-03').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            100: {
+                items: 1
+            },
+            425: {
+                items: 2
+            },
+            500: {
+                items: 2
+            },
+            768: {
+                items: 2
+            },
+            820: {
+                items: 3
+            },
+            1100: {
+                items: 4
+            },
+
+            1300: {
+                items: 5
+            }
+        }
+    })
+    $('.slider-04').owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: false,
+        dots: false,
+        ltr: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            100: {
+                items: 1
+            },
+            500: {
+                items: 1
+            },
+            768: {
+                items: 1
+            },
+
+            1100: {
+                items: 1
+            },
+
+            1300: {
+                items: 1
+            }
+        }
+    })
+    $('.slider-05').owlCarousel({
+        loop: true,
+        nav: false,
+        dots: false,
+        ltr: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            100: {
+                items: 2
+            },
+            425: {
+                items: 2
+            },
+            500: {
+                items: 2
+            },
+            768: {
+                items: 2
+            },
+            1024: {
+                items: 3
+            },
+            1100: {
+                items: 3
+            },
+
+            1300: {
+                items: 4
+            }
+        }
+    })
+    $('.slider-06').owlCarousel({
+        loop: true,
+        nav: false,
+        margin: 15,
+        dots: false,
+        ltr: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            100: {
+                items: 1
+            },
+          
+            500: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+
+            1100: {
+                items: 4
+            },
+
+            1300: {
+                items: 4
+            }
+        }
+    })
+    $('.slider-07').owlCarousel({
+        loop: true,
+        nav: false,
+        margin: 15,
+        dots: false,
+        ltr: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            100: {
+                items: 2
+            },
+            425: {
+                items: 2
+            },
+            500: {
+                items: 2
+            },
+            768: {
+                items: 4
+            },
+
+            1100: {
+                items: 4
+            },
+
+            1300: {
+                items: 4
+            }
+        }
+    })
